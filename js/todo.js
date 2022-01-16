e9bd74e43e578631c016ce3cf143c87e8db9c9d5
lyh0419 <lyh960419@icloud.com>
2022-01-16 18:44:59 +0900

    todo_add

--- js/todo.js
+++ js/todo.js
@@ -2,18 +2,28 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+//username 없으면 창 숨기기 시작
 const username = localStorage.getItem("username");
 if (username === null) {
   toDoForm.classList.add("hidden");
 } else if (username !== null) {
   toDoForm.classList.remove("hidden");
 }
+// username 없으면 창 숨기기 종료
+
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = newTodo;
+  toDoList.appendChild(li);
+}
 
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
-  console.log(newTodo);
+  paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
