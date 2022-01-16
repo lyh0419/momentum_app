2ad83c1f1abf65565912edc47618fa767ef12603
lyh0419 <lyh960419@icloud.com>
2022-01-16 19:35:42 +0900

    todo_save

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,14 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "toDos";
+
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
 //username 없으면 창 숨기기 시작
 const username = localStorage.getItem("username");
 if (username === null) {
@@ -11,11 +19,20 @@ if (username === null) {
 }
 // username 없으면 창 숨기기 종료
 
+function deleteTodo() {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
   span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteTodo);
+  li.appendChild(span);
+  li.appendChild(button);
   toDoList.appendChild(li);
 }
 
@@ -23,7 +40,17 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
+  toDos.push(newTodo);
   paintToDo(newTodo);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  console.log(parsedToDos);
+  parsedToDos.forEach((item) => console.log("this is the turn of", item));
+}
