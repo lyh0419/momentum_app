254703f2d2563de5ba584125a86dd651d9eccb4d
lyh0419 <lyh960419@icloud.com>
2022-01-17 01:45:52 +0900

    Weather_complete

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = "fdc3a2a39dbbba5394853a17887c3651";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const log = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+  alert("죄송합니다. 위치를 찾을 수 없습니다.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
